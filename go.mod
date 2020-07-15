a7208610af44564c199f8ac7406dc015382396e9
rudiarta <rudiarta321@gmail.com>
2020-07-15 20:26:48 +0800

    first init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module test/rudi
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/googollee/go-socket.io v1.4.3
+	github.com/gorilla/websocket v1.4.2
+	github.com/karlkfi/inject v0.0.0-20190704175410-86d2a962209d
+	github.com/stretchr/testify v1.4.0
+)
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type ItemData struct {
+	Type            string
+	Title           string
+	Brand           string
+	OperatingSystem string
+}
+
+type Inventory struct {
+	Item []ItemData
+}
+
+// Why this isn't constant because Go does not support struct constants (emphasis mine)
+// https://stackoverflow.com/questions/43368604/constant-struct-in-go/43368686
+var inventory = []Inventory{
+	{
+		[]ItemData{
+			{
+				Title: "Jurrasic Park",
+				Type:  "Book",
+			},
+			{
+				Brand: "Mercedes Benz",
+				Type:  "Car",
+			},
+			{
+				Type:            "Smartphone",
+				Brand:           "Samsung",
+				OperatingSystem: "Android",
+			},
+			{
+				Type:  "Car",
+				Brand: "Ferari",
+			},
+			{
+				Type:  "Book",
+				Title: "Harry Potter and The Chamber of Secret",
+			},
+		},
+	},
+	{
+		[]ItemData{
+			{
+				Type:  "Car",
+				Brand: "Tesla",
+			},
+			{
+				Type:            "Smartphone",
+				Brand:           "Apple",
+				OperatingSystem: "iOS",
+			},
+			{
+				Type:            "Smartphone",
+				Brand:           "Xiaomi",
+				OperatingSystem: "Android",
+			},
+			{
+				Type:  "Book",
+				Title: "Learning Data Mining with Python",
+			},
+		},
+	},
+}
+
+func main() {
+	result := GetResult()
+	fmt.Print(result)
+}
+
+func GetResult() string {
+	var tmpType []string
+	var tmpCount int = 0
+	var result string = ""
+	for _, items := range inventory {
+		CategorizeType(items.Item, &tmpType)
+	}
+
+	for i, tipe := range tmpType {
+		index := strconv.Itoa(i + 1)
+		result = result + index + " " + tipe + "\n"
+		for _, items := range inventory {
+			for _, item := range items.Item {
+				if tipe == item.Type {
+					tmpCount++
+
+					var value string = ExtractValue(item, item.Type)
+
+					count := strconv.Itoa(tmpCount)
+					result = result + index + "." + count + " " + value + "\n"
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func CategorizeType(items []ItemData, tmpType *[]string) {
+	for _, item := range items {
+		if !Find(*tmpType, item.Type) {
+			*tmpType = append(*tmpType, item.Type)
+		}
+	}
+}
+
+func ExtractValue(item ItemData, val string) string {
+	if val == "Book" {
+		val = item.Title
+	}
+
+	if val == "Car" {
+		val = item.Brand
+	}
+
+	if val == "Smartphone" {
+		val = item.Brand + " (" + item.OperatingSystem + ")"
+	}
+
+	return val
+}
+
+func Find(slice []string, val string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
